cd55ef22508fdfa195f56f5a1f35c0b85b7e44e0
Jacob Adlers <jacob.adlers@gmail.com>
2021-03-23 03:39:54 +0100

    Fix ln logging wrapped in []

--- logging.go
+++ logging.go
@@ -4,14 +4,14 @@ import "fmt"
 import "log"
 
 func logInfoln(a ...interface{}) {
-	log.Println("[INFO] ", a)
+	log.Printf("[INFO] %v\n", a...)
 }
 func logInfof(format string, a ...interface{}) {
 	log.Printf("[INFO] %s", fmt.Sprintf(format, a...))
 }
 
 func logDebugln(a ...interface{}) {
-	log.Println("[DEBUG] ", a)
+	log.Printf("[DEBUG] %v\n", a...)
 }
 
 func logDebugf(format string, a ...interface{}) {
