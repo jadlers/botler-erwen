c5c898367b5ba8da6a57106d2ae8fcf047da8171
Jacob Adlers <jacob.adlers@gmail.com>
2021-03-25 19:42:32 +0100

    Implement getLabel

--- bot/label.go
+++ bot/label.go
@@ -1,8 +1,16 @@
 package bot
 
-import "github.com/google/go-github/v33/github"
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/v33/github"
+)
 
 func (b *Bot) getLabel(name string) (*github.Label, error) {
-	b.log.Panic("NOT IMPLEMENTED")
-	return nil, nil
+	label, _, err := b.gh.Issues.GetLabel(context.Background(), b.conf.Owner, b.conf.Repository, name)
+	if err != nil {
+		return nil, fmt.Errorf("No label named '%s' exist in repository\n", name)
+	}
+	return label, nil
 }
